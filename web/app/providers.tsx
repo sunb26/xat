79b6267878e93f5f7dc01ca73c550dd59fdcaa34
Lev1ty <adamxhy123@outlook.com>
2024-03-21 00:50:45 -0400

    use fake clerk publishable key if it is missing in env

--- web/app/providers.tsx
+++ web/app/providers.tsx
@@ -4,10 +4,16 @@ import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+const FAKE_PUBLISHABLE_KEY =
+  "pk_test_d2VsY29tZS1zdGFyZmlzaC02NS5jbGVyay5hY2NvdW50cy5kZXYk";
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
+const PUBLISHABLE_KEY =
+  process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ??
+  // HACK: use fake publishable key when there is no publishable key
+  FAKE_PUBLISHABLE_KEY;
+
+if (PUBLISHABLE_KEY === FAKE_PUBLISHABLE_KEY) {
+  console.warn("using fake clerk publishable key");
 }
 
 export function Providers({ children }: { children: React.ReactNode }) {
