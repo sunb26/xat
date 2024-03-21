32005c04ac6f65aef5084132dec3342cfcb1030d
Zack-Ren <renzackary@gmail.com>
2024-03-21 00:29:53 -0400

    setup clerk

--- web/app/providers.tsx
+++ web/app/providers.tsx
@@ -2,8 +2,19 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+    </ClerkProvider>
+  );
 }
