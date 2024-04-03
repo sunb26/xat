100e6b02d98b19aae9eba38eb64469d8fc45cb0f
Ben Sun <sunb26@mcmaster.ca>
2024-04-02 20:47:34 -0400

    add money functions

---
+++ utils/money.go
@@ -0,0 +1,45 @@
+package money
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Money struct {
+	Dollars int
+	Cents   int
+}
+
+func ParseMoney(strInput string, money *Money) (err error) {
+
+	strInput = strings.TrimPrefix(strInput, "$")
+	strInput = strings.Replace(strInput, ",", "", -1)
+
+	parts := strings.Split(strInput, ".")
+	if len(parts) != 2 {
+		return fmt.Errorf("invalid money format")
+	}
+
+	dollars, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return fmt.Errorf("error parsing dollars: %v", err)
+	}
+
+	cents, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return fmt.Errorf("error parsing cents: %v", err)
+	}
+
+	money.Cents = cents
+	money.Dollars = dollars
+
+	return nil
+}
+
+func AddMoney(m1, m2 Money) Money {
+	totalCents := m1.Cents + m2.Cents
+	dollars := m1.Dollars + m2.Dollars + totalCents/100
+	cents := totalCents % 100
+	return Money{Dollars: dollars, Cents: cents}
+}
