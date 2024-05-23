4e1a3b0a2abe5ab22cc5276ef23688ba26f2db31
Zackary Ren <renzackary@gmail.com>
2024-05-22 23:25:53 -0400

    allow addRecipe to handle file or camera

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
             <NavBar />
             {children}
           </main>
+          <Toaster />
         </Providers>
       </body>
     </html>
--- web/components/addReceipt.tsx
+++ web/components/addReceipt.tsx
@@ -6,10 +6,10 @@ import {
   DrawerContent,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
 import { Button, Image } from "@nextui-org/react";
 import React, { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
-import { Input } from "@/components/ui/input";
 
 const videoConstraints = {
   width: 240,
@@ -21,6 +21,7 @@ export const AddReciept = () => {
   const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
   const webcamRef = useRef<Webcam>(null);
   const [url, setUrl] = useState<string | null>(null);
+  const [uploadedFile, setUploadedFile] = useState<FileList>();
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current?.getScreenshot();
     if (imageSrc) {
@@ -28,10 +29,22 @@ export const AddReciept = () => {
     }
     setCaptureEnable(false);
   }, []);
-  function resetCamera() {
+  function handleCancel() {
     setUrl(null);
-    setCaptureEnable(true);
+    setCaptureEnable(false);
+    setUploadedFile(undefined);
   }
+
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      setUploadedFile(e.target.files);
+    }
+  };
+
+  const handleFileUpload = async () => {
+    const file = uploadedFile?.[0];
+  };
 
   return (
     <Drawer>
@@ -39,49 +52,69 @@ export const AddReciept = () => {
         <Button>Add receipt</Button>
       </DrawerTrigger>
       <DrawerContent>
-        <div className="flex flex-col p-4 gap-4 min-h-96 mx-auto w-full max-w-md">
-          {!url && !isCaptureEnable && (
-            <div className="flex flex-col gap-6">
-              <Input type="file" />
-              <div className="text-lg font-semibold">or</div>
-              <Button onPress={() => setCaptureEnable(true)}>Take a picture</Button>
-            </div>
-          )}
-          {isCaptureEnable && (
-            <>
-              {/*
+        <div className="flex flex-col p-4 gap-4 min-h-96 mx-auto w-full max-w-md justify-between">
+          <div className="flex flex-col w-full gap-4">
+            {!url && !isCaptureEnable && (
+              <Input type="file" onChange={handleChange} />
+            )}
+            {!url && !uploadedFile && !isCaptureEnable && (
+              <div className="text-lg font-semibold mx-auto">or</div>
+            )}
+            {!uploadedFile && !isCaptureEnable && !url && (
+              <Button onPress={() => setCaptureEnable(true)}>
+                Take a picture
+              </Button>
+            )}
+            {isCaptureEnable && (
+              <>
+                {/*
             NOTE: https://github.com/microsoft/TypeScript/issues/31147
             // @ts-ignore*/}
-              <Webcam
-                audio={false}
-                width={240}
-                height={360}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                videoConstraints={videoConstraints}
-                className="rounded-md mx-auto"
-              />
-              <Button onPress={capture} color="primary">
-                Capture
-              </Button>
-            </>
-          )}
-          {url && (
-            <>
-              <div className="flex mx-auto">
-                <Image src={url} alt="receipt screenshot" />
-              </div>
-              <div className="flex">
-                <Button onPress={resetCamera} className="w-full">
-                  Re-take
+                <Webcam
+                  audio={false}
+                  width={240}
+                  height={360}
+                  ref={webcamRef}
+                  screenshotFormat="image/jpeg"
+                  videoConstraints={videoConstraints}
+                  className="rounded-md mx-auto"
+                />
+                <Button onPress={capture} color="primary">
+                  Capture
                 </Button>
-              </div>
-            </>
-          )}
-          <Button isDisabled={!url}>Continue</Button>
-          <DrawerClose asChild>
-            <Button variant="bordered">Cancel</Button>
-          </DrawerClose>
+              </>
+            )}
+            {url && (
+              <>
+                <div className="mx-auto">
+                  <Image src={url} alt="receipt screenshot" />
+                </div>
+                <div className="flex">
+                  <Button onPress={handleCancel} className="w-full">
+                    Re-take
+                  </Button>
+                </div>
+              </>
+            )}
+          </div>
+          <div className="flex gap-2">
+            <DrawerClose asChild>
+              <Button
+                variant="bordered"
+                onPress={handleCancel}
+                className="w-full"
+              >
+                Cancel
+              </Button>
+            </DrawerClose>
+            <Button
+              isDisabled={!url && !uploadedFile}
+              color="primary"
+              className="w-full"
+            >
+              Continue
+            </Button>
+          </div>
         </div>
       </DrawerContent>
     </Drawer>
