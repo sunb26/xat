ab91727f676bd01c683e002901308511da76003e
Zackary Ren <renzackary@gmail.com>
2024-06-13 22:39:12 -0400

    new columns for receipt table

--- web/components/receiptTable.tsx
+++ web/components/receiptTable.tsx
@@ -14,6 +14,7 @@ import {
   Card,
   CardBody,
   CardHeader,
+  Link,
   Popover,
   PopoverContent,
   PopoverTrigger,
@@ -24,20 +25,23 @@ import {
   TableColumn,
   TableHeader,
   TableRow,
+  Tooltip,
 } from "@nextui-org/react";
-import { History } from "lucide-react";
+import { History, FileImage } from "lucide-react";
 import { useCallback } from "react";
 
 const data = [
   {
     id: 1,
     gst: 13.0,
+    merchant: "McDonald's",
     date: new Date("2024-04-04T18:25:43.511Z"),
     total: 123.0,
     items: {
       subtotal: 100.0,
       gratuity: 10.0,
     },
+    image: "https://picsum.photos/seed/picsum/200/300",
     history: [
       {
         id: 2,
@@ -55,10 +59,11 @@ const data = [
 type Receipt = (typeof data)[0];
 
 const columns = [
+  { name: "MERCHANT", id: "merchant" },
   { name: "TOTAL", id: "total" },
   { name: "GST/HST", id: "gst" },
   { name: "DATE", id: "date" },
-  { name: "HISTORY", id: "history" },
+  { name: "ACTIONS", id: "actions" },
 ];
 type Column = (typeof columns)[0];
 
@@ -67,6 +72,8 @@ export const ReceiptTable = () => {
     const cellValue = data[columnKey as keyof Receipt];
 
     switch (columnKey) {
+      case "merchant":
+        return <div>{data.merchant}</div>;
       case "gst":
         return <div>${data.gst.toFixed(2)}</div>;
       case "date":
@@ -93,43 +100,58 @@ export const ReceiptTable = () => {
             </PopoverContent>
           </Popover>
         );
-      case "history":
+      case "actions":
         return (
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button isIconOnly variant="light">
-                <History className="w-5 h-5" />
+          <div className="flex gap-1">
+            <Tooltip content="View Receipt">
+              <Button
+                as={Link}
+                isIconOnly
+                variant="light"
+                href={data.image}
+                target="_blank"
+              >
+                <FileImage className="w-5 h-5" />
               </Button>
-            </SheetTrigger>
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Receipt History</SheetTitle>
-                <SheetDescription>
-                  Here are all the changes made to this receipt
-                </SheetDescription>
-              </SheetHeader>
-              <div className="h-full p-6">
-                {data.history.map((item) => (
-                  <div className="flex gap-4 mb-4" key={item.id}>
-                    <Slider
-                      isDisabled
-                      orientation="vertical"
-                      size="sm"
-                      color="foreground"
-                      maxValue={1}
-                      minValue={0}
-                      defaultValue={0.8}
-                      className="h-16"
-                    />
-                    <div className="flex flex-col gap-2">
-                      <div className="font-semibold">{item.date}</div>
-                      <div className="text-sm">{item.change}</div>
+            </Tooltip>
+            <Sheet>
+              <Tooltip content="Receipt History">
+                <SheetTrigger asChild>
+                  <Button isIconOnly variant="light">
+                    <History className="w-5 h-5" />
+                  </Button>
+                </SheetTrigger>
+              </Tooltip>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Receipt History</SheetTitle>
+                  <SheetDescription>
+                    Here are all the changes made to this receipt
+                  </SheetDescription>
+                </SheetHeader>
+                <div className="h-full p-6">
+                  {data.history.map((item) => (
+                    <div className="flex gap-4 mb-4" key={item.id}>
+                      <Slider
+                        isDisabled
+                        orientation="vertical"
+                        size="sm"
+                        color="foreground"
+                        maxValue={1}
+                        minValue={0}
+                        defaultValue={0.8}
+                        className="h-16"
+                      />
+                      <div className="flex flex-col gap-2">
+                        <div className="font-semibold">{item.date}</div>
+                        <div className="text-sm">{item.change}</div>
+                      </div>
                     </div>
-                  </div>
-                ))}
-              </div>
-            </SheetContent>
-          </Sheet>
+                  ))}
+                </div>
+              </SheetContent>
+            </Sheet>
+          </div>
         );
       default:
         return cellValue;
@@ -145,7 +167,9 @@ export const ReceiptTable = () => {
         <Table removeWrapper aria-label="Receipts table">
           <TableHeader columns={columns}>
             {(column: Column) => (
-              <TableColumn key={column.id}>{column.name}</TableColumn>
+              <TableColumn key={column.id} align="center">
+                {column.name}
+              </TableColumn>
             )}
           </TableHeader>
           <TableBody emptyContent={"No rows to display."} items={data}>
